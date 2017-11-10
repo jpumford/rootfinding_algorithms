0fcbc22140997041fb059e0018dc0d5cec8dbdf2
goggalore <junec177@gmail.com>
2017-11-09 18:36:59 -0700

    removed redundancy

--- secant/index.js
+++ secant/index.js
@@ -4,7 +4,6 @@ function secant(f, initial1, initial2, epsilon = 1e-7) {
     const finitial1 = f(initial1);
     const finitial2 = f(initial2);
     if (abs(finitial1) <= epsilon) return initial1;
-    if (abs(finitial1) <= epsilon) return initial2;
     const secantx = initial2 - (finitial2 * (initial2 - initial1)/(finitial2 - finitial1));
     return secant(f, initial2, secantx, epsilon);
 }
